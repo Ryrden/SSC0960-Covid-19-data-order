b574bf4017f8219936430d8585bcf07ba3810490
Ryan <ryansateles@gmail.com>
2022-07-16 00:57:14 -0300

    feature: all functions implementeds and  almost done

--- app.ts
+++ app.ts
@@ -8,25 +8,9 @@ import { CovidData } from "./covidData";
 2- Dentre os dez países com maiores valores de "Active", a soma dos "Deaths" dos cinco países com menores valres de "Confirmed".
 3- O maior valor de "Deaths" entre os países do hemisfério sul.
 4- O maior valor de "Deaths" entre os países do hemisfério norte.
-5- A soma de "Active" de todos os países em que "Confirmed" é maior o igual que 1.000.000.
+5- A soma de "Active" de todos os países em que "Confirmed" é maior ou igual que 1.000.000.
 */
 
-type CovidData = {
-	FIPS: string;
-	Admin2: string;
-	provinceState: string;
-	countryRegion: string;
-	lastUpdate: string;
-	lat: string;
-	long: string;
-	Confirmed: number;
-	Deaths: number;
-	recovered: number;
-	active: number;
-	combinedKey: string;
-	incidenceRate: string;
-	casefatalityRate: string;
-};
 //FIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,
 //Long_,Confirmed,Deaths,Recovered,Active,Combined_Key,Incident_Rate,Case_Fatality_Ratio
 const csvFilePath = path.resolve(__dirname, "./01-01-2021.csv");
@@ -55,24 +39,33 @@ parse(fileContent, {delimiter: ",", columns: headers}, (error, result: CovidData
 	if (error) {
 		console.error(error);
 	}
+	result.shift() //removing header result array
 
-	result.shift() //removing header from the result array
-
-	//ordena em ordem decrescente (parametro 'Country_Region' já em ordem alfabética). 
 	let threeBiggerConfirmedOnList: CovidData[] = result
-											.filter((a) => a.Confirmed > 0 )
 											.sort((a,b) =>  b.Confirmed - a.Confirmed)
-											.slice(0,3);
-	console.log(threeBiggerConfirmedOnList);
+											.slice(0,3)
 
-	let sumOfDeaths = 0;
-	result
-		.sort((a,b) => a.active - b.active)
-		.slice(0,10).sort((a,b) => b.Confirmed - a.Confirmed)
-		.slice(0,5)
-		.forEach((curr) => sumOfDeaths += curr.Deaths)
+	let sumOfDeaths: Number = result
+				.sort((a,b) => a.Active - b.Active)
+				.slice(0,10).sort((a,b) => b.Confirmed - a.Confirmed)
+				.slice(0,5)
+				.reduce((acc,curr) => +acc + +curr.Deaths,0)
 
-	//console.log(sumOfDeaths);
-
-
-});
+	let HigherSouthHemisphereDeaths = result.filter((data) => data.Lat < 0).sort((a,b) => b.Deaths - a.Deaths).slice(0,1);
+	
+	let HigherNorthHemisphereDeaths = result.filter((data) => data.Lat > 0).sort((a,b) => b.Deaths - a.Deaths).slice(0,1);
+	
+	let ActiveSumWhereConfirmedCountrysIsBiggerThan1000000: number = result.reduce((acc,curr) => {
+		if (curr.Confirmed >= 1_000_000)
+		return +acc + +curr.Active
+		return +acc;
+	},0)
+	
+	
+	
+	console.log("1-",threeBiggerConfirmedOnList)
+	console.log("2-",sumOfDeaths);
+	console.log("3-",HigherSouthHemisphereDeaths)
+	console.log("4-",HigherNorthHemisphereDeaths)
+	console.log("5-",ActiveSumWhereConfirmedCountrysIsBiggerThan1000000)
+})
--- covidData.ts
+++ covidData.ts
@@ -1,17 +1,17 @@
 export interface CovidData {
 	FIPS: string;
 	Admin2: string;
-	ProvinceState: string;
-	CountryRegion: string;
-	LastUpdate: string;
-	Lat: string;
-	Long: string;
+	Province_State: string;
+	Country_Region: string;
+	Last_Update: string;
+	Lat: number;
+	Long: number;
 	Confirmed: number;
 	Deaths: number;
 	Recovered: number;
 	Active: number;
-	CombinedKey: string;
-	IncidenceRate: string;
-	CasefatalityRate: string;
+	Combined_Key: string;
+	Incidence_Rate: string;
+	Casefatality_Rate: string;
 };
 
